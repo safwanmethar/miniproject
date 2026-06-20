716d9f27209c37551470fe9b2cda0e5f10f61bbf
safwanmethar <safwanmethar002@gmail.com>
2026-06-21 00:56:04 +0530

    Fix Django template directory for Vercel

--- mini/settings.py
+++ mini/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'mini.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
